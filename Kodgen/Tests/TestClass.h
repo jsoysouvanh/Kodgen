6f0cc91bb55306049ec31bfd5799880df447e835
Julien Soysouvanh <j.soysouvanh@student.isartdigital.com>
2020-03-21 15:34:47 +0100

    [Fix] Rename in tests

--- Kodgen/Tests/TestClass.h
+++ Kodgen/Tests/TestClass.h
@@ -11,32 +11,32 @@ namespace TestNamespace
 {
 	class Class1{};
 
-	class RfrkClass(GenTemplate[TestTemplate], ClassProp1, ClassProp2[ClassSubProp21, ClassSubProp22], ClassProp3) TestClass final : public Class1
+	class KGClass(GenTemplate[TestTemplate], ClassProp1, ClassProp2[ClassSubProp21, ClassSubProp22], ClassProp3) TestClass final : public Class1
 	{
 		//_RFRK_GENERATED_TestClass
 
 		private:
 			#pragma region Variables
 
-			RfrkField(FieldProp1, FieldProp2[FieldSubProp21, FieldSubProp22], FieldProp3)
+			KGField(FieldProp1, FieldProp2[FieldSubProp21, FieldSubProp22], FieldProp3)
 			const volatile int* const&	var1;
 
 			#pragma endregion
 
 		protected:
-			RfrkField(FieldProp2[])
+			KGField(FieldProp2[])
 			class Hey*		var4 = nullptr;
 	
 		public:
-			RfrkField(FieldProp1)
+			KGField(FieldProp1)
 			int var2 : 1;
 
 			#pragma region Methods
 
-			RfrkMethod(MethodProp1, MethodProp2[MethodSubProp21, MethodSubProp22], MethodProp3)
+			KGMethod(MethodProp1, MethodProp2[MethodSubProp21, MethodSubProp22], MethodProp3)
 			virtual const volatile int* const& someMethod(int param1, class Hey* param2) const final;
 
-			RfrkMethod(MethodProp1, MethodProp2[MethodSubProp21, MethodSubProp22], MethodProp3)
+			KGMethod(MethodProp1, MethodProp2[MethodSubProp21, MethodSubProp22], MethodProp3)
 			virtual int someMethod2(int param1, class Hey* param2) final
 			{
 
@@ -52,9 +52,9 @@ namespace TestNamespace
 			float const		var3;
 	};
 
-	enum class RfrkEnum(GenTemplate[TestTemplate], EnumProp1, EnumProp2[EnumSubProp21, EnumSubProp22], EnumProp3) MyEnum : uint8_t
+	enum class KGEnum(GenTemplate[TestTemplate], EnumProp1, EnumProp2[EnumSubProp21, EnumSubProp22], EnumProp3) MyEnum : uint8_t
 	{
-		EVal1 RfrkEnumVal(EnumValueProp1, EnumValueProp2[EnumValueSubProp21, EnumValueSubProp22], EnumValueProp3) = 1 << 0,
+		EVal1 KGEnumVal(EnumValueProp1, EnumValueProp2[EnumValueSubProp21, EnumValueSubProp22], EnumValueProp3) = 1 << 0,
 		EVal2 = 1 << 1,
 		a,
 		b,
@@ -62,14 +62,14 @@ namespace TestNamespace
 		c
 	};
 
-	class RfrkClass() TestClass2
+	class KGClass() TestClass2
 	{
 		//_RFRK_GENERATED_TestClass2
 
 		private:
 			#pragma region Variables
 
-			RfrkField()
+			KGField()
 			int				var1 = 42;
 			float			var2;
 
@@ -80,7 +80,7 @@ namespace TestNamespace
 			public:
 			#pragma region Methods
 
-			RfrkMethod()
+			KGMethod()
 			int someMethod(int);
 			int dsomeMethod3(int) noexcept;
 			virtual int someMethodd3(int) const;
